c0d8777bde0df80b926e11d74300b47e0b186a53
Denis <denis.melenevsky@gmail.com>
2026-07-29 20:06:55 +0300

    Initial Commit

---
+++ DeploymentTargetTest/DeploymentTargetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeploymentTargetTestApp.swift
+//  DeploymentTargetTest
+//
+//  Created by Denis on 29.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct DeploymentTargetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeploymentTargetTestTests/DeploymentTargetTestTests.swift
@@ -0,0 +1,19 @@
+//
+//  DeploymentTargetTestTests.swift
+//  DeploymentTargetTestTests
+//
+//  Created by Denis on 29.07.2026.
+//
+
+import Testing
+@testable import DeploymentTargetTest
+
+struct DeploymentTargetTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
