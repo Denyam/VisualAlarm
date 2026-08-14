44e79853897e6f7cfb776a8ed5493916a8fcaf76
Denis <denis.melenevsky@gmail.com>
2026-08-14 18:50:02 +0300

    Initial Commit

---
+++ VisualAlarm/VisualAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualAlarmApp.swift
+//  VisualAlarm
+//
+//  Created by Denis on 14.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct VisualAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisualAlarmTests/VisualAlarmTests.swift
@@ -0,0 +1,16 @@
+//
+//  VisualAlarmTests.swift
+//  VisualAlarmTests
+//
+//  Created by Denis on 14.08.2026.
+//
+
+import Testing
+
+struct VisualAlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
